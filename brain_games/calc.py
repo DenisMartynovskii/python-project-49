878c4093d9c495e14e70431015e4ec14978fa05d
Nikolaytk87 <42153211+Nikolaytk87@users.noreply.github.com>
2019-11-16 12:49:45 +0300

    Edit Step 6

--- brain_games/calc.py
+++ brain_games/calc.py
@@ -20,17 +20,13 @@ def get_expression_result(items):
     return result
 
 
-def get_result_calc():
+def get_result():
     (num_1, num_2, sign) = generate_random()
     result = get_expression_result((num_1, num_2, sign))
     print("Question: {} {} {}".format(num_1, sign, num_2))
     return result
 
 
-def is_even(num):
-    return "yes" if (num % 2 == 0) else "no"
-
-
 def run_game():
     user_name = greet_user("What is the result of the expression?")
-    flow_game(get_result_calc, user_name)
+    flow_game(get_result, user_name)
