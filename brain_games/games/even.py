64a19b370644e69669bff275c3385bfef4c5ca5f
Nikolay <nick@tt.local>
2019-11-20 11:03:18 +0300

    Corrrection step 3

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -2,7 +2,7 @@ from random import randint
 from brain_games.templates import flow_game, greet_user
 
 
-def get_result():
+def is_even():
     random_num = randint(0, 100)
     result = "yes" if (random_num % 2 == 0) else "no"
     print("Question: ", random_num)
@@ -12,4 +12,4 @@ def get_result():
 def run_game():
     case = 'Answer "yes" if number even otherwise answer "no".'
     name = greet_user(case)
-    flow_game(get_result, name)
+    flow_game(is_even, name)
