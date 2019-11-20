65f062e43e2ccfdc6a0b897992caae3d3eaba8a5
Nikolay <nick@tt.local>
2019-11-20 10:57:28 +0300

    Corrrection step 1,2

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -2,18 +2,9 @@ from random import randint
 from brain_games.templates import flow_game, greet_user
 
 
-def generate_random():
-    random_item = randint(0, 100)
-    return random_item
-
-
-def get_expression_result(num):
-    return "yes" if (num % 2 == 0) else "no"
-
-
 def get_result():
-    random_num = generate_random()
-    result = get_expression_result(random_num)
+    random_num = randint(0, 100)
+    result = "yes" if (random_num % 2 == 0) else "no"
     print("Question: ", random_num)
     return result
 
