f959ddeeab711c61b8d0fabe3633abf2919f14f9
Denis <Denis_mart@mail.ru>
2023-05-23 20:50:47 +0300

    main

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ https://asciinema.org/a/v9gq5IA5hkP6mjYvj4KDBKgJP
 https://asciinema.org/connect/04cc64b4-b0e2-44df-aa6e-e20794805eb6
 https://asciinema.org/connect/04cc64b4-b0e2-44df-aa6e-e20794805eb6
 https://asciinema.org/connect/04cc64b4-b0e2-44df-aa6e-e20794805eb6
+https://asciinema.org/connect/04cc64b4-b0e2-44df-aa6e-e20794805eb6
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,56 @@
+from random import randint
+
+def main():
+    def correct_1(answer):
+        print ('correct!')
+        num = randint(1, 100)
+        prime = num > 1 and (num % 2 != 0 or num == 2) and (num % 3 != 0 or num == 3)
+        i = 5;
+        d = 2;
+        while prime and i * i <= num:
+            prime = num % i != 0
+            i += d
+            d = 6 - d
+        print (f'Question: {num}')
+        answer = (input('Your answer: '))
+        if prime == True and answer == 'yes': print(f'Correct! \nCongratulations, {name}!')
+        elif prime == False and answer == 'no': print(f'Correct! \nCongratulations, {name}!')
+        elif prime == False and answer == 'yes': print(f"'yes' is wrong answer ;(. Correct answer was 'no'. \nLet's try again, {name}!")
+        else: print (f"'no' is wrong answer ;(. Correct answer was 'yes'. \nLet's try again, {name}!")
+    def correct(answer):
+        print ('correct!')
+        num = randint(1, 100)
+        prime = num > 1 and (num % 2 != 0 or num == 2) and (num % 3 != 0 or num == 3)
+        i = 5;
+        d = 2;
+        while prime and i * i <= num:
+            prime = num % i != 0
+            i += d
+            d = 6 - d
+        print (f'Question: {num}')
+        answer = (input('Your answer: '))
+        if prime == True and answer == 'yes': return correct_1('yes')
+        elif prime == False and answer == 'no': return correct_1('yes')
+        elif prime == False and answer == 'yes': print(f"'yes' is wrong answer ;(. Correct answer was 'no'. \nLet's try again, {name}!")
+        else: print (f"'no' is wrong answer ;(. Correct answer was 'yes'. \nLet's try again, {name}!")
+    print(f'Welcome to the Brain Games!')
+    name =''
+    while name == '':
+        print('May I have your name? ', end='')
+        name = input()
+    print(f'Hello, {name}!')
+    print(f'Answer "yes" if given number is prime. Otherwise answer "no".')
+    num = randint(1, 100)
+    prime = num > 1 and (num % 2 != 0 or num == 2) and (num % 3 != 0 or num == 3)
+    i = 5;
+    d = 2;
+    while prime and i * i <= num:
+        prime = num % i != 0
+        i += d
+        d = 6 - d
+    print (f'Question: {num}')
+    answer = (input('Your answer: '))
+    if prime == True and answer == 'yes': return correct('yes')
+    elif prime == False and answer == 'no': return correct('yes')
+    elif prime == False and answer == 'yes': print(f"'yes' is wrong answer ;(. Correct answer was 'no'. \nLet's try again, {name}!")
+    else: print (f"'no' is wrong answer ;(. Correct answer was 'yes'. \nLet's try again, {name}!")
