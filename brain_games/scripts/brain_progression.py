3160121ebbfe623b35c82f7b4fc0eb91f3ef5cc2
DenisMartynovskii <129895503+DenisMartynovskii@users.noreply.github.com>
2023-05-27 22:56:26 +0300

    Update brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,65 +1,31 @@
 from random import randint
+import prompt
+
+
+count = 3
 
 
 def main():
-
-    def correct_1(answer):
-        print('correct!')
-        progression_length = 10
-        start = randint(1, 100)
-        step = randint(1, 10)
-        miss_item_index = randint(1, progression_length - 1)
-        end = start + (progression_length * step)
-        progression = list(range(start, end, step))
-        result = progression.pop(miss_item_index)
-        progression.insert(miss_item_index, "..")
-        question = " ".join([str(i) for i in progression])
-        print(f'Question: {question}')
-        answer = int(input('Your answer: '))
-        if answer == result:
-            print(f'Correct! \nCongratulations, {name}!')
-        else:
-            print(f"{answer} is wrong answer ;(. Correct answer was {result}."
-                  f"\nLet's try again, {name}!")
-
-    def correct(answer):
-        print('correct!')
-        progression_length = 10
-        start = randint(1, 100)
-        step = randint(1, 10)
-        miss_item_index = randint(1, progression_length - 1)
-        end = start + (progression_length * step)
-        progression = list(range(start, end, step))
-        result = progression.pop(miss_item_index)
-        progression.insert(miss_item_index, "..")
-        question = " ".join([str(i) for i in progression])
-        print(f'Question: {question}')
-        answer = int(input('Your answer: '))
-        if answer == result:
-            return correct_1('answer')
-        else:
-            print(f"{answer} is wrong answer ;(. Correct answer was {result}."
-                  f"\nLet's try again, {name}!")
     print('Welcome to the Brain Games!')
-    name = ''
-    while name == '':
-        print('May I have your name? ', end='')
-        name = input()
+    name = prompt.string("May I have your name? ")
     print(f'Hello, {name}!')
-    print('What number is missing in the progression?')
-    progression_length = 10
-    start = randint(1, 100)
-    step = randint(1, 10)
-    miss_item_index = randint(1, progression_length - 1)
-    end = start + (progression_length * step)
-    progression = list(range(start, end, step))
-    result = progression.pop(miss_item_index)
-    progression.insert(miss_item_index, "..")
-    question = " ".join([str(i) for i in progression])
-    print(f'Question: {question}')
-    answer = int(input('Your answer: '))
-    if answer == result:
-        return correct('answer')
-    else:
-        print(f"{answer} is wrong answer ;(. Correct answer was {result}."
-              f"\nLet's try again, {name}!")
+    for i in range(count):
+        print('What number is missing in the progression?')
+        progression_length = 10
+        start = randint(1, 100)
+        step = randint(1, 10)
+        miss_item_index = randint(1, progression_length - 1)
+        end = start + (progression_length * step)
+        progression = list(range(start, end, step))
+        result = progression.pop(miss_item_index)
+        progression.insert(miss_item_index, "..")
+        question = " ".join([str(i) for i in progression])
+        print(f'Question: {question}')
+        answer = int(input('Your answer: '))
+        if answer == result:
+            print("Correct!")
+        else:
+            print(f"{answer} is wrong answer ;(. Correct answer was {result}."
+                  f"\nLet's try again, {name}!")
+            return
+    print(f'Congratulations, {name}!')
