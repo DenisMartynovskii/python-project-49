73c67a7d83b3eac624ef591bc4cf705dbe35da79
DenisMartynovskii <129895503+DenisMartynovskii@users.noreply.github.com>
2023-05-27 22:47:53 +0300

    Update brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,48 +1,26 @@
 from random import randint
 from math import gcd
+import prompt
+
+
+count = 3
 
 
 def main():
-
-    def correct_1(answer):
-        print('correct!')
-        number = randint(1, 100)
-        number_1 = randint(1, 100)
-        print(f'Question: {number} {number_1}')
-        answer = int(input('Your answer: '))
-        result = gcd(number, number_1)
-        if answer == result:
-            print(f'Correct! \nCongratulations, {name}!')
-        else:
-            print(f"{answer} is wrong answer ;(. Correct answer was {result}."
-                  f"\nLet's try again, {name}!")
-
-    def correct(answer):
-        print('correct!')
-        number = randint(1, 100)
-        number_1 = randint(1, 100)
-        print(f'Question: {number} {number_1}')
-        answer = int(input('Your answer: '))
-        result = gcd(number, number_1)
-        if answer == result:
-            return correct_1(answer)
-        else:
-            print(f"{answer} is wrong answer ;(. Correct answer was {result}."
-                  f"\nLet's try again, {name}!")
     print('Welcome to the Brain Games!')
-    name = ''
-    while name == '':
-        print('May I have your name? ', end='')
-        name = input()
+    name = prompt.string("May I have your name? ")
     print(f'Hello, {name}!')
-    print('Find the greatest common divisor of given numbers.')
-    number = randint(1, 100)
-    number_1 = randint(1, 100)
-    print(f'Question: {number} {number_1}')
-    answer = int(input('Your answer: '))
-    result = gcd(number, number_1)
-    if answer == result:
-        return correct(answer)
-    else:
-        print(f"{answer} is wrong answer ;(. Correct answer was {result}."
-              f"\nLet's try again, {name}!")
+    for i in range(count):
+        print('Find the greatest common divisor of given numbers.')
+        number = randint(1, 100)
+        number_1 = randint(1, 100)
+        print(f'Question: {number} {number_1}')
+        answer = int(input('Your answer: '))
+        result = gcd(number, number_1)
+        if answer == result:
+            print("Correct!")
+        else:
+            print(f"{answer} is wrong answer ;(. Correct answer was {result}."
+                  f"\nLet's try again, {name}!")
+            return
+    print(f'Congratulations, {name}!')
