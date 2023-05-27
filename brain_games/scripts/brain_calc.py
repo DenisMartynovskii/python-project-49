b6074f59c3690ff5fc6fe00c1d20ee64ee65950b
DenisMartynovskii <129895503+DenisMartynovskii@users.noreply.github.com>
2023-05-27 22:11:20 +0300

    Update brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,75 +1,35 @@
-from random import choice
 from random import randint
+from random import choice
+import prompt
+
+
+count = 3
 
 
 def main():
-
-    def correct_1():
-        print('correct!')
-        number = randint(0, 10)
-        number_1 = randint(0, 10)
-        arithmetic = ['+', '-', '*']
-        arithm = choice(arithmetic)
-        if arithm == '+':
-            print(f'Question: {number} {arithm} {number_1}')
-            result = number + number_1
-        if arithm == '-':
-            print(f'Question: {number} {arithm} {number_1}')
-            result = number - number_1
-        if arithm == '*':
-            print(f'Question: {number} {arithm} {number_1}')
-            result = number * number_1
-        answer = int(input('Your answer: '))
-        if answer == result:
-            print(f'Correct! \nCongratulations, {name}!')
-        else:
-            print(f"{answer} is wrong answer ;(. Correct answer was {result}."
-                  f"\nLet's try again, {name}!")
-
-    def correct():
-        print('correct!')
-        number = randint(0, 10)
-        number_1 = randint(0, 10)
-        arithmetic = ['+', '-', '*']
-        arithm = choice(arithmetic)
-        if arithm == '+':
-            print(f'Question: {number} {arithm} {number_1}')
-            result = number + number_1
-        if arithm == '-':
-            print(f'Question: {number} {arithm} {number_1}')
-            result = number - number_1
-        if arithm == '*':
-            print(f'Question: {number} {arithm} {number_1}')
-            result = number * number_1
-        answer = int(input('Your answer: '))
-        if answer == result:
-            return correct_1()
-        else:
-            print(f"{answer} is wrong answer ;(. Correct answer was {result}."
-                  f"\nLet's try again, {name}!")
     print('Welcome to the Brain Games!')
-    name = ''
-    while name == '':
-        print('May I have your name? ', end='')
-        name = input()
+    name = prompt.string("May I have your name? ")
     print(f'Hello, {name}!')
-    print('What is the result of the expression?')
-    number = randint(0, 10)
-    number_1 = randint(0, 10)
-    arithmetic = ['+', '-', '*']
-    arithm = choice(arithmetic)
-    if arithm == '+':
-        print(f'Question: {number} {arithm} {number_1}')
-        result = number + number_1
-    if arithm == '-':
-        print(f'Question: {number} {arithm} {number_1}')
-        result = number - number_1
-    if arithm == '*':
-        print(f'Question: {number} {arithm} {number_1}')
-        result = number * number_1
-    answer = int(input('Your answer: '))
-    if answer == result:
-        return correct()
-    else:
-        print(f"{answer} is wrong answer ;(. Correct answer was {result}."
-              f"\nLet's try again, {name}!")
+    for i in range(count):
+        print('What is the result of the expression?')
+        number = randint(0, 10)
+        number_1 = randint(0, 10)
+        arithmetic = ['+', '-', '*']
+        arithm = choice(arithmetic)
+        if arithm == '+':
+            print(f'Question: {number} {arithm} {number_1}')
+            result = number + number_1
+        if arithm == '-':
+            print(f'Question: {number} {arithm} {number_1}')
+            result = number - number_1
+        if arithm == '*':
+            print(f'Question: {number} {arithm} {number_1}')
+            result = number * number_1
+        answer = int(input('Your answer: '))
+        if answer == result:
+            print("Correct!")
+        else:
+            print(f"{answer} is wrong answer ;(. Correct answer was {result}."
+                  f"\nLet's try again, {name}!")
+            return
+    print(f'Congratulations, {name}!')
