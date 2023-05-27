4e30b92592a77311ee8cebf1929f426ce0ad3a3f
DenisMartynovskii <129895503+DenisMartynovskii@users.noreply.github.com>
2023-05-27 22:37:41 +0300

    Update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,55 +1,29 @@
 from random import randint
+import prompt
+
+
+count = 3
 
 
 def main():
-    def correct_1(answer):
-        print('correct')
-        number = randint(1, 1000)
-        print(f'Question: {number}')
-        answer = (input('Your answer: '))
-        if number % 2 == 0 and answer == 'yes':
-            print(f'Correct! \nCongratulations, {name}!')
-        elif number % 2 != 0 and answer == 'no':
-            print(f'Correct! \nCongratulations, {name}!')
-        elif number % 2 == 0 and answer == 'no':
-            print(f"'no' is wrong answer ;(. Correct answer was 'yes'."
-                  f"\nLet's try again, {name}!")
-        else:
-            print(f"'yes' is wrong answer ;(. Correct answer was 'no'."
-                  f"\nLet's try again, {name}!")
-
-    def correct(answer):
-        print('correct')
-        number = randint(1, 1000)
-        print(f'Question: {number}')
-        answer = (input('Your answer: '))
-        if number % 2 == 0 and answer == 'yes':
-            return correct_1('yes')
-        elif number % 2 != 0 and answer == 'no':
-            return correct_1('no')
-        elif number % 2 == 0 and answer == 'no':
-            print(f"'no' is wrong answer ;(. Correct answer was 'yes'."
-                  f"\nLet's try again, {name}!")
-        else:
-            print(f"'yes' is wrong answer ;(. Correct answer was 'no'."
-                  f"\nLet's try again, {name}!")
     print('Welcome to the Brain Games!')
-    name = ''
-    while name == '':
-        print('May I have your name? ', end='')
-        name = input()
+    name = prompt.string("May I have your name? ")
     print(f'Hello, {name}!')
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    number = randint(1, 1000)
-    print(f'Question: {number}')
-    answer = (input('Your answer: '))
-    if number % 2 == 0 and answer == 'yes':
-        return correct('yes')
-    elif number % 2 != 0 and answer == 'no':
-        return correct('no')
-    elif number % 2 == 0 and answer == 'no':
-        print(f"'no' is wrong answer ;(. Correct answer was 'yes'."
-              f"\nLet's try again, {name}!")
-    else:
-        print(f"'yes' is wrong answer ;(. Correct answer was 'no'."
-              f"\nLet's try again, {name}!")
+    for i in range(count):
+        print('Answer "yes" if the number is even, otherwise answer "no".')
+        number = randint(1, 1000)
+        print(f'Question: {number}')
+        answer = (input('Your answer: '))
+        if number % 2 == 0 and answer == 'yes':
+            print("Correct!")
+        elif number % 2 != 0 and answer == 'no':
+            print("Correct!")
+        elif number % 2 == 0 and answer == 'no':
+            print(f"'no' is wrong answer ;(. Correct answer was 'yes'."
+                  f"\nLet's try again, {name}!")
+            return
+        else:
+            print(f"'yes' is wrong answer ;(. Correct answer was 'no'."
+                  f"\nLet's try again, {name}!")
+            return
+    print(f'Congratulations, {name}!')
