de2db44cdbb4a60c9a8d857af3ffadb8a26769f9
DenisMartynovskii <129895503+DenisMartynovskii@users.noreply.github.com>
2023-05-29 23:08:39 +0300

    Update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,12 @@
 from random import randint
-from scripts.greet import greet
+import promt
 
 count = 3
 
 def main():
-    greet()
+    print('Welcome to the Brain Games!')
+    name = prompt.string("May I have your name? ")
+    print(f'Hello, {name}!')
     for i in range(count):
         print('Answer "yes" if the number is even, otherwise answer "no".')
         number = randint(1, 1000)
