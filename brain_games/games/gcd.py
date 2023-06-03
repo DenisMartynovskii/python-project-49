e06e5670aeedbfc5881a5c3e43a2889b9e383d7e
DenisMartynovskii <129895503+DenisMartynovskii@users.noreply.github.com>
2023-06-03 22:46:55 +0300

    Update gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -15,7 +15,7 @@ def get_gcd(num1, num2):
 
 def get_quiz():
     num1, num2 = choices(range(1, 30), k=2)
-    question = num1, num2
+    question = f"{num1} {num2}"
     answer = get_gcd(num1, num2)
     return question, str(answer)
 
