fdced3479d20ec4f5c26268d3c99e88acc673842
DenisMartynovskii <129895503+DenisMartynovskii@users.noreply.github.com>
2023-06-05 21:40:55 +0300

    Update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,8 +1,9 @@
-from brain_games.games import even
+from brain_games.flow import flow_game
+from brain_games.games.even import get_quiz
 
 
 def main():
-    even.run_game()
+    flow_game(get_quiz)
 
 
 if __name__ == "__name__":
