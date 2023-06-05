e036a17619898f2ad87b0a6d6269fa4c64e8fa1f
DenisMartynovskii <129895503+DenisMartynovskii@users.noreply.github.com>
2023-06-05 21:41:40 +0300

    Update brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,8 +1,9 @@
-from brain_games.games import gcd
+from brain_games.flow import flow_game
+from brain_games.games.gcd import get_quiz
 
 
 def main():
-    gcd.run_game()
+    flow_game(get_quiz)
 
 
 if __name__ == "__name__":
