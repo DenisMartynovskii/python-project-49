a9d4ae3ecae0ca9a2563dd0026063ea4473c2897
DenisMartynovskii <129895503+DenisMartynovskii@users.noreply.github.com>
2023-06-05 21:22:52 +0300

    Update calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,6 +1,5 @@
 from random import choice, choices
-from brain_games.flow import flow_game
-from brain_games.games import calc
+
 
 QUIZ = "What is the result of the expression?"
 
@@ -23,7 +22,3 @@ def get_quiz():
     answer = calculated_expression(num1, num2, sign)
     question = f"{num1} {sign} {num2}"
     return question, str(answer)
-
-
-def main():
-    flow_game(calc)
